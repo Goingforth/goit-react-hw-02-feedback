808f4d35bd670a144b76dc970f700f3b9104bde7
Oleksandr Siukov <as-service2008@ukr.net>
2023-05-02 12:58:37 +0300

    html каркас

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+export const App = () => {
+  return (
+    <div>
+      <h3>Please leave feedback</h3>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <h3>Statistics</h3>
+      <ul>
+        <li>
+          <span>Good:</span>
+          <span>3</span>
+        </li>
+        <li>
+          <span>Neutral:</span>
+          <span>2</span>
+        </li>
+        <li>
+          <span>Bad:</span>
+          <span>2</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
